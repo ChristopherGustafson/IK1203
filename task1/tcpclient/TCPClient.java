c08901e2cb353eed218a2eb5b2e41e0cbaa8ed66
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-02-11 15:36:29 +0100

    Added exceptionhandling at timeout

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -6,16 +6,12 @@ public class TCPClient {
 
     public static String askServer(String hostname, int port, String ToServer) throws  IOException {
 
+      if(ToServer == null){
+        return askServer(hostname, port);
+      }
       //Create client socket and set timeout to 5s
       Socket client = new Socket(hostname, port);
       client.setSoTimeout(5000);
-
-      //Create output and input streams
-      DataOutputStream output = new DataOutputStream(client.getOutputStream());
-      BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
-      //Write user input to server
-      output.writeBytes(ToServer + "\n");
 
       String s;
       StringBuilder sb = new StringBuilder("");
@@ -24,15 +20,30 @@ public class TCPClient {
       int count = 0;
       int maxLineCount = 100;
 
-      //Read lines from server until null is read or the max line count is reached
-      while(((s = input.readLine()) != null) && count <= maxLineCount){
-        sb.append(s);
-        count++;
-      }
 
-      //Close socket and return string representation of the StringBuilder
-      client.close();
-      return sb.toString();
+      try{
+        //Create output and input streams
+        DataOutputStream output = new DataOutputStream(client.getOutputStream());
+        BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        //Write user input to server
+        output.writeBytes(ToServer + "\n");
+
+        //Read lines from server until null is read or the max line count is reached
+        while(((s = input.readLine()) != null) && count <= maxLineCount){
+          sb.append(s + '\n');
+          count++;
+        }
+
+        //Close socket and return string representation of the StringBuilder
+        client.close();
+        return sb.toString();
+      }
+      // If a error occurs (like a timeout), close the socket and return what we have recieved thus far
+      catch(IOException e){
+        client.close();
+        return sb.toString();
+      }
     }
 
     public static String askServer(String hostname, int port) throws  IOException {
@@ -41,10 +52,6 @@ public class TCPClient {
       Socket client = new Socket(hostname, port);
       client.setSoTimeout(5000);
 
-      //Create output and input streams
-      DataOutputStream output = new DataOutputStream(client.getOutputStream());
-      BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
       String s;
       StringBuilder sb = new StringBuilder("");
 
@@ -52,14 +59,26 @@ public class TCPClient {
       int count = 0;
       int maxLineCount = 100;
 
-      //Read lines from server and append to StringBuilder until null is read or the max line count is reached
-      while(((s = input.readLine()) != null) && count <= maxLineCount){
-        sb.append(s);
-        count++;
-      }
+      try{
 
-      //Close socket and return string representation of the StringBuilder
-      client.close();
-      return sb.toString();
+        //Create output and input streams
+        DataOutputStream output = new DataOutputStream(client.getOutputStream());
+        BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        //Read lines from server and append to StringBuilder until null is read or the max line count is reached
+        while(((s = input.readLine()) != null) && count <= maxLineCount){
+          sb.append(s + '\n');
+          count++;
+        }
+
+        //Close socket and return string representation of the StringBuilder
+        client.close();
+        return sb.toString();
+      }
+      // If a error occurs (like a timeout), close the socket and return what we have recieved thus far
+      catch(IOException e){
+        client.close();
+        return sb.toString();
+      }
     }
 }
