fd6f970e70157406da44a84e3cf17877a87562f6
Christopher Gustafson <christopher.gustafson@outlook.com>
2019-02-14 09:27:57 +0100

    Added task2, HTTPEcho server

---
+++ task2/HTTPEcho.java
@@ -0,0 +1,23 @@
+import java.net.*;
+import java.io.*;
+
+public class HTTPEcho{
+  public static void main( String[] args) throws IOException{
+
+    ServerSocket initSocket = new ServerSocket(Integer.parseInt(args[0]));
+    String HTTPHeader = "HTTP/1.1 200 OK\r\n\r\n";
+    while(true){
+
+      Socket connection = initSocket.accept();
+      BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      DataOutputStream output = new DataOutputStream(connection.getOutputStream());
+      StringBuilder sb = new StringBuilder(HTTPHeader);
+      String s = "";
+      while((s = input.readLine()) != null && s.length() != 0){
+        sb.append(s + "\n");
+      }
+      output.writeBytes(sb.toString());
+      connection.close();
+    }
+  }
+}
